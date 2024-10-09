bfb6a27a81ae2fdcb9c2f9deee895ba45313c50a
Alejandro Martin Martin <alejandro.martin@viewnext.com>
2024-10-09 11:01:13 +0200

    branch

--- src/Main/main.java
+++ src/Main/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Que bien?");
+		System.out.println("Que finnnnnnnnn?");
 
 	}
 
